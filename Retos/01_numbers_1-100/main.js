4ca5fcfc3882d7f5f94ffae2f951e107a3d9f9b4
Cristian Penagos <suarezalejo26@gmail.com>
2025-06-19 07:14:59 -0500

    ready

--- Retos/01_numbers_1-100/main.js
+++ Retos/01_numbers_1-100/main.js
@@ -1,26 +1,25 @@
-let listNumbers = document.getElementById('listNumbers');
+let listNumbers = document.getElementById("listNumbers");
 
 for (let i = 1; i <= 100; i++) {
+  let contentP = "";
+  let newElementP = document.createElement("p");
 
-    let contentP = '';
-    let newElementP = document.createElement('p');
-
-    if (i%3 == 0 && i%5 == 0) {
-        contentP = 'FizzBuzz';
-        newElementP.textContent = contentP;
-        newElementP.style.color = "rgb(255, 255, 255)"
-    } else if (i%5 == 0) {
-        contentP = 'Buzz';
-        newElementP.textContent = contentP;
-        newElementP.style.color = "rgb(255, 255, 255)"
-    } else if (i%3 == 0) {
-        contentP = 'Fizz';
-        newElementP.textContent = contentP;
-        newElementP.style.color = "rgb(255, 255, 255)"
-    } else {
-        contentP = i;
-        newElementP.textContent = contentP;
-        newElementP.style.color = "rgb(242, 232, 207)"
-    }   
-    listNumbers.appendChild(newElementP);
+  if (i % 3 == 0 && i % 5 == 0) {
+    contentP = "FizzBuzz";
+    newElementP.textContent = contentP;
+    newElementP.style.color = "rgb(255, 255, 255)";
+  } else if (i % 5 == 0) {
+    contentP = "Buzz";
+    newElementP.textContent = contentP;
+    newElementP.style.color = "rgb(255, 255, 255)";
+  } else if (i % 3 == 0) {
+    contentP = "Fizz";
+    newElementP.textContent = contentP;
+    newElementP.style.color = "rgb(255, 255, 255)";
+  } else {
+    contentP = i;
+    newElementP.textContent = contentP;
+    newElementP.style.color = "rgb(242, 232, 207)";
+  }
+  listNumbers.appendChild(newElementP);
 }
