39ade76c1326cc2ed48dbd5c68f71847ce2a9aef
Cristian Penagos <suarezalejo26@gmail.com>
2025-06-19 06:36:16 -0500

    ready

--- Retos/01_numbers_1-100/main.js
+++ Retos/01_numbers_1-100/main.js
@@ -1,16 +1,23 @@
-let containerNumbers = document.getElementById("numbers")
+let putNumber = document.getElementById('numbers');
 
-const showNumbers = () =>{
-    for (let counter = 1; counter<=100; counter++) {
+for (let index = 1; index <= 100; index++) {
 
-/*         if (counter%3 == 0) && (counter%5 == 0){
-            
-        } */
+    let infoText = '';
 
-        console.log(counter)
-        
+    if (index % 3 === 0 && index % 5 === 0) {
+        infoText = 'FizzBuzz';
+    } else if (index % 5 === 0) {
+        infoText = 'Buzz';
+    } else if (index % 3 === 0) {
+        infoText = 'Fizz';
+    } else {
+        infoText = index;
     }
 
-}
+    /* console.log(index)  */
 
-showNumbers()
+    // Agrego el texto al DOM
+    const newElement = document.createElement('p');   // crea un "objeto" que representa el nuevo párrafo
+    newElement.textContent = infoText;                // Este paso le da contenido al párrafo que acabas de crear
+    putNumber.appendChild(newElement);                // Agrega ese párrafo al contenedor en la página
+}
